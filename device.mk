e9b5cff1e1c85f6d7de1171c42608116f388a86d
Andreas Gampe <agampe@google.com>
2016-06-02 10:05:12 -0700

    veux: Pull in Otapreopt
    Add otapreopt packages to marlin and sailfish. This prepares
    for A/B OTA dexopting.
    
    Bug: 25612095
    Change-Id: I4fcf64fdb9933f3a3a52fce76264e352f6e774f6

--- device.mk
+++ device.mk
@@ -15,6 +15,12 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 # A/B
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
 
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
 # Soong
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
