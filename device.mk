714531da8493e65914766e7bb2f90b9f6789c2a9
Lokesh Gidra <lokeshgidra@google.com>
2019-12-06 13:19:19 -0800

    veux: Setup 6GB Dalvik heap configuration
    Change-Id: Ia2b826c95643f935878335c2434deadf7f8401c4

--- device.mk
+++ device.mk
@@ -9,6 +9,9 @@ $(call inherit-product, hardware/qcom-caf/common/common.mk)
 # Enable project quotas and casefolding for emulated storage without sdcardfs
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
+# Setup dalvik vm configs
+$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
+
 # Soong
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
