f53b3ff02b6bacd4840644fb057348e312de402d
Leo Liou <leoliou@google.com>
2020-01-14 17:59:51 +0800

    veux: Inherit the virtual A/B config
    For devices launching with virtual A/B, inherit the virtual A/B
    device base configuration.
    
    Bug: 146466421
    Test: Build and boot & manual OTA.
    Change-Id: I752178c2525335b3cebafa0254c077d93831b8ba

--- device.mk
+++ device.mk
@@ -12,6 +12,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
+# A/B
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
+
 # Soong
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
