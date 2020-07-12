1d3aee807832d9e58fe39b8fe12045ff77d66a56
Demon000 <demonsingur@gmail.com>
2020-07-12 13:04:04 +0300

    veux: Build telephony packages
    Change-Id: I5fafc22fc696b04abc2f79100306f1625ac03fbc

--- device.mk
+++ device.mk
@@ -350,5 +350,18 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/google/pixel \
     hardware/xiaomi
 
+# Telephony
+PRODUCT_PACKAGES += \
+    ims-ext-common \
+    ims_ext_common.xml \
+    qti-telephony-hidl-wrapper \
+    qti_telephony_hidl_wrapper.xml \
+    qti-telephony-utils \
+    qti_telephony_utils.xml \
+    telephony-ext
+
+PRODUCT_BOOT_JARS += \
+    telephony-ext
+
 # Inherit from proprietary targets
 $(call inherit-product, vendor/xiaomi/veux/veux-vendor.mk)
