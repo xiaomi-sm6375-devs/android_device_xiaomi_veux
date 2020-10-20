699d604b33394fb4ecec2f41f0858c23654ca6af
Volodymyr Zhdanov <wight554@gmail.com>
2020-10-20 10:52:41 +0300

    veux: Build IPACM from source
    Change-Id: Id40cea7059316370fc18dfbdb9e00b5b84f28b10

--- device.mk
+++ device.mk
@@ -151,6 +151,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
      IFAAService
 
+# IPACM
+PRODUCT_PACKAGES += \
+    ipacm \
+    IPACM_cfg.xml
+
 # Soong
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
