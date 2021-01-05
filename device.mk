06bec00be8dca99ddfe4cc3b9dafba469af8efff
LuK1337 <priv.luk@gmail.com>
2021-01-05 13:42:00 +0100

    veux: Build QTI bootctrl HAL
    Change-Id: I1e907c64ee4f88507bac78afe56e8fba0310f6d1

--- device.mk
+++ device.mk
@@ -78,6 +78,11 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_holi/bluetooth_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_holi/r_submix_audio_policy_configuration.xml
 
+# Boot control
+PRODUCT_PACKAGES += \
+    android.hardware.boot-service.qti \
+    android.hardware.boot-service.qti.recovery
+
 # Soong
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
