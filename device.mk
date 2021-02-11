ddeeb7eb8712cd163f84238037bb4c1c9cddd1ed
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-11 11:02:49 +0100

    veux: Initial lineage-sdk overlays
    Change-Id: I2aaa0c06b23b40aac120c325ce15fb0992463520

--- device.mk
+++ device.mk
@@ -19,7 +19,8 @@ TARGET_SCREEN_WIDTH := 1080
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 # Inherit from vendor blobs
 $(call inherit-product, vendor/xiaomi/veux/veux-vendor.mk)
