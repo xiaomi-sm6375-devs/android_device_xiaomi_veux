c793b4c659e75a8e64dcf06c621cfee7b1432a14
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-04-21 22:15:07 +0200

    veux: Use QTI USB init configs from USB HAL repo
    Change-Id: I966d251a2756c9d9e91f621cbc5872448d022de4

--- device.mk
+++ device.mk
@@ -282,7 +282,8 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/google/pixel \
     hardware/lineage/interfaces/power-libperfmgr \
     hardware/qcom-caf/common/libqti-perfd-client \
-    hardware/xiaomi
+    hardware/xiaomi \
+    vendor/qcom/opensource/usb/etc
 
 # Telephony
 PRODUCT_PACKAGES += \
@@ -318,6 +319,10 @@ PRODUCT_PACKAGES += \
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb-service.qti
+
+PRODUCT_PACKAGES += \
+    init.qcom.usb.rc \
+    init.qcom.usb.sh
 
 # Inherit from proprietary targets
 $(call inherit-product, vendor/xiaomi/veux/veux-vendor.mk)
