d27baed9f4b6a14d191dd8e2fd11ff04d5b60464
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-07-24 06:25:46 +0200

    veux: Build QTI Framework detect libs
    Change-Id: Ic4db96f8bd5a19f4805bd9c40544ed51d0352c61

--- device.mk
+++ device.mk
@@ -315,6 +315,12 @@ PRODUCT_PACKAGES += \
     libprotobuf-cpp-full-3.9.1-vendorcompat \
     libprotobuf-cpp-lite-3.9.1-vendorcompat
 
+# QMI
+PRODUCT_PACKAGES += \
+    libjson \
+    libqti_vndfwk_detect.vendor \
+    libvndfwk_detect_jni.qti.vendor
+
 # Soong
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
