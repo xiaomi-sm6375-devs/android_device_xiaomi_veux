804202210db1b16cf7d8a727ce8d7d716112a9c5
LuK1337 <priv.luk@gmail.com>
2022-10-15 23:05:18 +0200

    veux: Prevent adding same dependency twice in blob_fixup
    Change-Id: Idedeb51e027a09f64a29ac67a3a2f2a0594213f6

--- extract-files.sh
+++ extract-files.sh
@@ -74,7 +74,7 @@ function blob_fixup() {
             ;;
         vendor/lib64/camera/components/com.qti.node.mialgocontrol.so)
             llvm-strip --strip-debug "${2}"
-            "${PATCHELF}" --add-needed "libpiex_shim.so" "${2}"
+            grep -q "libpiex_shim.so" "${2}" || "${PATCHELF}" --add-needed "libpiex_shim.so" "${2}"
             ;;
     esac
 }
