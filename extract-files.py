72deaeed9ef5b9cea5b51f79a3a2dfec1f4755f0
althafvly <althafvly@gmail.com>
2022-11-19 12:45:23 +0530

    veux: Add libpiex_shim for com.mi.node.mialgocontrol.so
    Change-Id: I9e8b125ab5dfaa325db63b02a7fcf51577ee7b0d

--- extract-files.py
+++ extract-files.py
@@ -46,6 +46,8 @@ lib_fixups: lib_fixups_user_type = {
 blob_fixups: blob_fixups_user_type = {
     'system_ext/lib64/libwfdnative.so': blob_fixup()
         .add_needed('libinput_shim.so'),
+    'vendor/lib64/camera/components/com.qti.node.mialgocontrol.so': blob_fixup()
+        .add_needed('libpiex_shim.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
