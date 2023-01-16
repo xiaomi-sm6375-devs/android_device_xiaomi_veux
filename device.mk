5c5e891c3d08c9830f03a24ab7b6bc53d02ca934
Ramii Ahmed <ramy@ahmedramy.com>
2023-01-16 03:05:12 +0000

    veux: Use libhidlbase from vndk v32 for vendor/lib64/libvendor.goodix.hardware.biometrics.fingerprint@2.1.so
    ["_ZN7android8hardware7details17gBnConstructorMapE" and "_ZN7android8hardware7details17gBsConstructorMapE" have been removed on Android 13]
    
    [@Joker-V2: Adapt to our blob]
    
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>
    Signed-off-by: Joker-V2 <nipinna0@gmail.com>
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>
    Change-Id: Ia052716c998d296e5f966abb2d0944a69ffa5f2e

--- device.mk
+++ device.mk
@@ -17,6 +17,9 @@ PRODUCT_AAPT_PREF_CONFIG := xhdpi
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/libhidlbase.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libhidlbase-v32.so
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
--- extract-files.sh
+++ extract-files.sh
@@ -14,6 +14,10 @@ function blob_fixup() {
         vendor/lib64/camera/components/com.qti.node.mialgocontrol.so)
             llvm-strip --strip-debug  "${2}"
             ;;
+        vendor/lib64/libvendor.goodix.hardware.biometrics.fingerprint@2.1.so)
+            "${PATCHELF_0_8}" --remove-needed "libhidlbase.so" "${2}"
+            sed -i "s/libhidltransport.so/libhidlbase-v32.so\x00/" "${2}"
+            ;;
     esac
 }
 
