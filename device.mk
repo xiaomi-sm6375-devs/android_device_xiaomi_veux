afd7f74ad453cdeb56f3f17427a27fdf3675b6a7
Ramii Ahmed <ramy@ahmedramy.com>
2023-01-16 05:30:14 +0000

    veux: namespace the tree

--- device.mk
+++ device.mk
@@ -29,5 +29,9 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
 # Inherit from vendor blobs
 $(call inherit-product, vendor/xiaomi/veux/veux-vendor.mk)
