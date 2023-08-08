38dd683b70479e91014e78fa86a8080ed23e1d3b
Alexander Winkowski <dereference23@outlook.com>
2023-08-08 06:12:11 +0000

    veux: Add target CPU flags
    Change-Id: Idd9d9689889a94016c93ac71dd2d44f277463231

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -8,3 +8,15 @@ DEVICE_PATH := device/xiaomi/veux
 
 BUILD_BROKEN_DUP_RULES := true
 BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-2a-dotprod
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_VARIANT := cortex-a55
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-2a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a55
