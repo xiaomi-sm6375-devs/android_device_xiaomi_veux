605babd6784902600e168dbc72ce094d7a9f6b2f
Alexander Winkowski <dereference23@outlook.com>
2023-08-12 07:26:32 +0000

    veux: configs: Add initial powerhint for blair
    Based on redbull, adapted nodes, removed unused/unneeded hints.
    
    Change-Id: Iee610168800d9cbecccc4ca121418a9bce717cce

--- device.mk
+++ device.mk
@@ -307,6 +307,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.power-service.xiaomi-libperfmgr
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
 # Soong
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
