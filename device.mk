10c3c61ae404928109b0d69f1a8913aa77f81815
Alexander Winkowski <dereference23@outlook.com>
2023-08-12 07:26:32 +0000

    veux: configs: Add initial powerhint for blair
    Based on redbull, adapted nodes, removed unused/unneeded hints.
    
    Change-Id: Iee610168800d9cbecccc4ca121418a9bce717cce

--- device.mk
+++ device.mk
@@ -253,6 +253,9 @@ PRODUCT_PACKAGES += \
     android.hardware.power-service.lineage-libperfmgr \
     libqti-perfd-client
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
 # Soong
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
