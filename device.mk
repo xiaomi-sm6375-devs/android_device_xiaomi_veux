ed169a33385397f6086b6bb2e43805804ed92b18
Alexander Winkowski <dereference23@outlook.com>
2023-08-25 06:56:34 +0000

    veux: Import display config
    From OS1.0.12.0.TKCEUXM
    
    Change-Id: I7017b425c091a7ae67adb0e6e9509f49dd314442

--- device.mk
+++ device.mk
@@ -102,6 +102,9 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.graphics.mapper@4.0-impl-qti-display:64
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/display_id_4630947081618265473.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4630947081618265473.xml
+
 # Soong
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
