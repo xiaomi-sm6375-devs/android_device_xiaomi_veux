689bbb984b1afed6025949df30d24aadce59faeb
Alexander Winkowski <dereference23@outlook.com>
2023-08-25 13:11:13 +0000

    veux: Set device name as per the stock ROM
    Change-Id: I611e5f912096b3853a7d80f4135656d20bbf7f26

--- device.mk
+++ device.mk
@@ -206,7 +206,16 @@ PRODUCT_PACKAGES += \
     SystemUIOverlayVEUX \
     WifiOverlayVEUX
 
+PRODUCT_PACKAGES += \
+    SettingsProviderOverlay2201116SG \
+    SettingsProviderOverlay2201116SC \
+    SettingsProviderOverlay2201116SI \
+    SettingsProviderOverlay2201116SR \
+    SettingsProviderOverlay2201116PG \
+    SettingsProviderOverlay2201116PI
+
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rro_overlays/config-odm.xml:$(TARGET_COPY_OUT_ODM)/overlay/config/config.xml \
     $(LOCAL_PATH)/rro_overlays/config-vendor.xml:$(TARGET_COPY_OUT_VENDOR)/overlay/config/config.xml
 
 # Partitions
