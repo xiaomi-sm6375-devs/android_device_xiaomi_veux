91ad7a5c76ad3a2d28d32b7af12d54d8da569c26
Aaron Kling <webgeek1234@gmail.com>
2023-08-29 15:58:31 -0500

    veux: Add vendor soong imports to extract
    Change-Id: Ib19e69dc35af6b04519eeb695228af65f3d01d2a

--- extract-files.py
+++ extract-files.py
@@ -9,9 +9,21 @@ from extract_utils.main import (
     ExtractUtilsModule,
 )
 
+namespace_imports = [
+    'device/xiaomi/veux',
+    'hardware/qcom-caf/sm8350',
+    'hardware/qcom-caf/wlan',
+    'hardware/xiaomi',
+    'vendor/qcom/opensource/commonsys/display',
+    'vendor/qcom/opensource/commonsys-intf/display',
+    'vendor/qcom/opensource/dataservices',
+    'vendor/qcom/opensource/display',
+]
+
 module = ExtractUtilsModule(
     'veux',
     'xiaomi',
+    namespace_imports=namespace_imports,
 )
 
 if __name__ == '__main__':
