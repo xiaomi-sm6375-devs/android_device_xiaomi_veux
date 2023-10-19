9107edf6184a15fca5cff24561b21e9f7a761ee6
LuK1337 <priv.luk@gmail.com>
2023-10-20 00:53:15 +0200

    veux: Build libprotobuf-cpp-lite-3.9.1-vendorcompat
    Change-Id: I0aac742db9c2b4b3e48ee20158f19405863893c2

--- device.mk
+++ device.mk
@@ -312,7 +312,8 @@ PRODUCT_COPY_FILES += \
 
 # Protobuf
 PRODUCT_PACKAGES += \
-    libprotobuf-cpp-full-3.9.1-vendorcompat
+    libprotobuf-cpp-full-3.9.1-vendorcompat \
+    libprotobuf-cpp-lite-3.9.1-vendorcompat
 
 # Soong
 PRODUCT_SOONG_NAMESPACES += \
