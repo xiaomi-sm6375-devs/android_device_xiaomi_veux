b5d275e1b5be3aa29876ad17597f4017596a21fa
Giovanni Ricca <giovanniricca@proton.me>
2023-11-30 18:23:43 +0100

    veux: Build FPC interface from source
    Change-Id: I80dd66139d1e53873beb14f7937cbfb721e582ca

--- device.mk
+++ device.mk
@@ -138,6 +138,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.3-service.xiaomi
 
+PRODUCT_PACKAGES += \
+    com.fingerprints.extension@1.0.vendor
+
 # Soong
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
