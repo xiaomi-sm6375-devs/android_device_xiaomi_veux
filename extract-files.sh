5b1eaa419575a050ac92c7e96dace0679081016c
Alexander Winkowski <dereference23@outlook.com>
2024-01-08 07:05:38 +0000

    veux: Add SKU props from stock
    Change-Id: Ib7b6a0d8b992f264762c21f477c42f2114e51c7f

--- extract-files.sh
+++ extract-files.sh
@@ -53,6 +53,15 @@ if [ -z "${SRC}" ]; then
     SRC="adb"
 fi
 
+function blob_fixup() {
+    case "${1}" in
+        odm/etc/build_*.prop)
+            sed -i "/marketname/d" "${2}"
+            sed -i "s/cert/model/" "${2}"
+            ;;
+    esac
+}
+
 # Initialize the helper
 setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
 
