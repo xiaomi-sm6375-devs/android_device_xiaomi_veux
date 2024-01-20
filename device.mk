57ac7743f0458df5cab624470b1cf4de06630507
Alexander Winkowski <dereference23@outlook.com>
2024-01-20 10:49:36 +0000

    veux: Update WFD blobs
    Change-Id: I663d4132969fc50c117ac29760b36536269712c2

--- device.mk
+++ device.mk
@@ -468,7 +468,8 @@ PRODUCT_COPY_FILES += \
 # WiFi Display
 PRODUCT_PACKAGES += \
     libnl \
-    libwfdaac_vendor
+    libwfdaac_vendor \
+    vendor.qti.hardware.display.config-V5-ndk
 
 PRODUCT_BOOT_JARS += \
     WfdCommon
