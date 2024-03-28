47b4fb705ffeab4b53e34ef57dca60152f476799
Bruno Martins <bgcngm@gmail.com>
2024-03-28 14:12:52 +0000

    veux: Mark CNE symlink target as required package
    Change-Id: I0bfe65ba61b6aa9916fe6459be2a84d772939a2f

--- device.mk
+++ device.mk
@@ -290,6 +290,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
+# QTI fwk-detect
+PRODUCT_PACKAGES += \
+    libvndfwk_detect_jni.qti.vendor # Needed by CNE app
+
 # Rootdir
 PRODUCT_PACKAGES += \
     fstab.default \
