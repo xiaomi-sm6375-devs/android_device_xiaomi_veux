cc48d215b024ef31b39709fc5b7549c925d10b51
danielml <daniel@danielml.dev>
2024-06-16 10:30:40 +0200

    veux: Build xiaomi-telephony-stub
    To provide the required Xiaomi IMS methods
    
    Change-Id: I1e75476a0d2ed55b5eae2a0a2c6ca4c3c4e2c0f1

--- device.mk
+++ device.mk
@@ -300,10 +300,12 @@ PRODUCT_PACKAGES += \
     qti-telephony-utils-prd \
     qti_telephony_utils.xml \
     qti_telephony_utils_prd.xml \
-    telephony-ext
+    telephony-ext \
+    xiaomi-telephony-stub
 
 PRODUCT_BOOT_JARS += \
-    telephony-ext
+    telephony-ext \
+    xiaomi-telephony-stub
 
 # Inherit from proprietary targets
 $(call inherit-product, vendor/xiaomi/veux/veux-vendor.mk)
