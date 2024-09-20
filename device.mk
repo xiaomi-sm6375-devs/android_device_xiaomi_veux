486c1ff40e49a77ce0beea900bb1f8d057a5343b
Alexander Winkowski <dereference23@outlook.com>
2024-09-20 05:16:34 +0000

    veux: Build 32-bit audio HALs
    Change-Id: I90e112f045b8e792f3bdd39ea6c4a4ec4a99e944

--- device.mk
+++ device.mk
@@ -34,6 +34,38 @@ PRODUCT_PACKAGES += \
 BOARD_SHIPPING_API_LEVEL := 30
 PRODUCT_SHIPPING_API_LEVEL := 30
 
+# Audio
+PRODUCT_PACKAGES += \
+    android.hardware.audio.service
+
+PRODUCT_PACKAGES += \
+    android.hardware.audio@6.0-impl:32 \
+    android.hardware.audio.effect@6.0-impl:32 \
+    android.hardware.bluetooth.audio-impl:32 \
+    android.hardware.soundtrigger@2.3-impl:32
+
+PRODUCT_PACKAGES += \
+    audio.bluetooth.default:32 \
+    audio.primary.holi:32 \
+    audio.r_submix.default:32 \
+    audio.usb.default:32
+
+PRODUCT_PACKAGES += \
+    audioadsprpcd \
+    liba2dpoffload:32 \
+    libbatterylistener:32 \
+    libcomprcapture:32 \
+    libexthwplugin:32 \
+    libhdmiedid:32 \
+    libhfp:32 \
+    libqcompostprocbundle:32 \
+    libqcomvisualizer:32 \
+    libqcomvoiceprocessing:32 \
+    libsndmonitor:32 \
+    libspkrprot:32 \
+    libvolumelistener:32 \
+    sound_trigger.primary.holi:32
+
 # Soong
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
