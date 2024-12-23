618dfe7682c3cc6f31224b63ecf81e15e7677a8b
Alexander Winkowski <dereference23@outlook.com>
2024-12-23 07:13:47 +0000

    veux: Patch libgoodixhwfingerprint to use OSS interface
    Change-Id: I08823428a08369fedf822171780ff511bc236c84

--- extract-files.py
+++ extract-files.py
@@ -56,6 +56,8 @@ blob_fixups: blob_fixups_user_type = {
         .clear_symbol_version('AHardwareBuffer_lock')
         .clear_symbol_version('AHardwareBuffer_release')
         .clear_symbol_version('AHardwareBuffer_unlock'),
+    'vendor/lib64/libgoodixhwfingerprint.so': blob_fixup()
+        .replace_needed('libvendor.goodix.hardware.biometrics.fingerprint@2.1.so', 'vendor.goodix.hardware.biometrics.fingerprint@2.1.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
