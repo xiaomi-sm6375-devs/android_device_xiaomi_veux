9ed4cc32edff08b04904345f0a11207396226961
Alexander Winkowski <dereference23@outlook.com>
2025-05-04 21:25:52 +0000

    veux: Don't leak IMEI to props
    Change-Id: I700c0985d40084300cafba9864cddd1772580228

--- extract-files.py
+++ extract-files.py
@@ -95,6 +95,8 @@ blob_fixups: blob_fixups_user_type = {
         .add_needed('libcrypto_shim.so'),
     'vendor/lib64/android.hardware.secure_element@1.0-impl.so': blob_fixup()
         .remove_needed('android.hidl.base@1.0.so'),
+    'vendor/lib64/libqcrilNr.so': blob_fixup()
+        .replace_needed('ro.ril.oem', ''),
     ('vendor/lib64/libalLDC.so', 'vendor/lib64/libalhLDC.so'): blob_fixup()
         .clear_symbol_version('AHardwareBuffer_allocate')
         .clear_symbol_version('AHardwareBuffer_describe')
