6cf3e4c48967e8ee56c19b35fc8f8a191476abd4
basamaryan <basam.aryan@gmail.com>
2025-12-13 18:02:38 -0800

    veux: overlay: Reduce blur radius
    Change-Id: Id6339df82156099ca14da684f236931b3ecf51ce

--- device.mk
+++ device.mk
@@ -205,6 +205,7 @@ PRODUCT_COPY_FILES += \
 # Overlays
 PRODUCT_PACKAGES += \
     CarrierConfigOverlayCOMMON \
+    Launcher3QuickStepOverlayCOMMON \
     LineageSettingsProviderOverlayCOMMON \
     SettingsOverlayCOMMON \
     TelephonyOverlayCOMMON
